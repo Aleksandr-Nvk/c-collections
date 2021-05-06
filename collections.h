d8ba98b27fb31ec6bd98b30ed7f2df19adba624c
Aleksandr Novik <alexandr.novik2@gmail.com>
2021-05-06 21:03:05 +0300

    Removing and freeing functions for List added

--- collections.h
+++ collections.h
@@ -2,10 +2,10 @@
 
 /* basic list structure, a wrapping around an array */
 typedef struct list_struct {
-    void** array; /* inner array */
+    void **array;           /* inner array */
 
-    size_t count; /* amount of items currently stored in array */
-    size_t capacity; /* possible amount of items stored in the array (can be expanded) */
+    size_t count;            /* amount of items currently stored in array */
+    size_t capacity;            /* possible amount of items stored in the array (can be expanded) */
 } list;
 
 /* creates a new list with zero capacity and returns it */
@@ -15,7 +15,7 @@ list new_list(void) {
 }
 
 /* returns an item from list by index or NULL if index is out of range */
-void* index_of_list(list* list, size_t index) {
+void *index_of_list(list *list, size_t index) {
     if (index >= list->capacity) {         /* Throw the error if index is out of range */
         perror("INDEX WAS OUT OF RANGE");
         return NULL;
@@ -25,14 +25,49 @@ void* index_of_list(list* list, size_t index) {
 }
 
 /* adds item to list */
-void add_to_list(list* list, void* item) {
+void add_to_list(list *list, void *item) {
     if (list->capacity == 0) {          /* allocate initial 4 bytes if empty */
         list->capacity = 4;
-        list->array = calloc(list->capacity, 1); /* used calloc to remove garbage values */
+        list->array = calloc(list->capacity, 1);            /* used calloc to remove garbage values */
     } else if (list->count == list->capacity) {         /* double the size if needed */
         list->capacity *= 2;
         list->array = realloc(list->array, list->capacity);
     }
 
     (*list).array[list->count++] = item;
+}
+
+/* removes the first occurrence of item from list */
+void remove_from_list(list *list, void *item) {
+    for (int i = 0; i < list->count; ++i) {
+        if (list->array[i] == item) {
+            for (int k = i; k < list->count; ++k) {     /* Defragmentation of list */
+                list->array[k] = list->array[k + 1];
+            }
+            --list->count;
+            break;
+        }
+    }
+}
+
+/* removes all items from list, keeps retains capacity */
+void remove_all_from_list(list *list) {
+    for (int i = 0; i < list->count; ++i) {
+        list->array[i] = NULL;
+    }
+    list->count = 0;
+}
+
+/* removes all items from list, sets capacity to 0 */
+void clear_list(list* list) {
+    list->count = 0;
+    list->capacity = 0;
+
+    free(list->array);
+    list->array = malloc(0);
+}
+
+/* frees memory occupied by list */
+void dealloc_list(list* list) {
+    free(list->array);
 }
--- main.c
+++ main.c
@@ -14,10 +14,22 @@ int main(void) {
         add_to_list(&my_list, &string[i]);
     }
 
-    for (int i = 0; i < my_list.count; ++i) {   /* printing the items of the list */
+    clear_list(&my_list);           /* practically recreating the list */
+
+    for (int i = 0; i < strlen(string); ++i) {   /* placing characters into the list again */
+        add_to_list(&my_list, &string[i]);
+    }
+
+    remove_from_list(&my_list, index_of_list(&my_list, 4));    /* remove an item with index '4' */
+
+    for (int i = 0; i < my_list.count; ++i) {    /* printing the items of the list */
         char* c = index_of_list(&my_list, i);
         printf("%c", *c);
     }
+
+    remove_all_from_list(&my_list);         /* nullify all items */
+
+    dealloc_list(&my_list);         /* free memory used by the list */
 
     return 0;
 }
